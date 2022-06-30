6a4d5d0cf2ac33567eab90a258902208c8699bfc
eminent alpaca <kristin@northcountrychapel.com>
2022-06-30 11:36:54 -0700

    reverse chronological order

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from dateutil import parser
 import psycopg2
 from sqlalchemy.orm import Session
 from sqlalchemy import desc, func
-from . import models
+from . import models, schemas 
 from .config import settings
 from .database import engine, get_db
 import datetime
@@ -36,7 +36,7 @@ def get_programs(response: Response, db: Session= Depends(get_db)):
     List all Apply Within programs. Returned in reverse chronological order from today's date.
     """
 
-    programs = db.query(models.APWI).order_by(desc(models.APWI.airdate)).filter(models.APWI.airdate <= today).all()
+    programs = db.query(models.APWI).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).all()
 
     if not programs:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
@@ -46,8 +46,13 @@ def get_programs(response: Response, db: Session= Depends(get_db)):
 #get list of programs by title
 @app.get("/v1/books/{search}", summary="Get programs by book title")
 def get_titles(search: str, response: Response, db: Session= Depends(get_db)):
+    """
+    List all Apply Within programs with search term in title.
+    """
+    
 
-    programs = db.query(models.APWI).filter(models.APWI.title.ilike('%'+ search +'%')).all()
+
+    programs = db.query(models.APWI).filter(models.APWI.title.ilike('%'+ search +'%')).order_by(desc(models.APWI.airdate)).all()
     
     if not programs:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
@@ -59,23 +64,25 @@ def get_titles(search: str, response: Response, db: Session= Depends(get_db)):
 def get_network(network, response: Response, db: Session= Depends(get_db)):
     """
     List programs by network (CALVARY or ACN). Returned in reverse chronological order from today's date.
+
+    For future programs use /v1/{network}/{start_date}?end_date=
     """
 
     network = network.upper()
 
-    programs = db.query(models.APWI).filter(models.APWI.network == network).order_by(desc(models.APWI.airdate)).filter(models.APWI.airdate <= today).all()
+    programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).all()
 
     if not programs:
-        raise HTTPException(status_code=status.HTTP_418_IM_A_TEAPOT, detail="Database did not return any programs")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
     return {"programs" : programs}
 
 #get list of programs on a network by date
 @app.get("/v1/{network}/{start_date}", summary="Get programs by network and date")
-def get_network_dates (network: str,  response: Response, db: Session= Depends(get_db), start_date: Union[str, None] = Query(default=Required, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"), end_date: Union[str, None] = Query(default=None, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")):
+def get_network_dates (network: str,  response: Response, db: Session= Depends(get_db), start_date: str = Query(default=Required, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"), end_date: Union[str, None] = Query(default=None, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")):
     """
     Get programs on a specific network (CALVARY or ACN) by date (YYYY-MM-DD).  
     
-    To find a range add ?end_date="YYYY-MM-DD" to query. Ordered in chronological order.
+    To return a range add ?end_date="YYYY-MM-DD" to query. Returned in reverse chronological order.
     """
     network = network.upper()
     #gotta have range
@@ -83,7 +90,7 @@ def get_network_dates (network: str,  response: Response, db: Session= Depends(g
         end_date = start_date
       
 
-    programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate.between(start_date,end_date)).all()
+    programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate.between(start_date,end_date)).order_by(desc(models.APWI.airdate)).all()
 
     if not start_date:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Date not in correct format. Please use YYYY-MM-DD.")
