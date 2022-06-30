91149c7b95430ae90787d534af23f24938d358cc
eminent alpaca <kristin@northcountrychapel.com>
2022-06-30 15:00:03 -0700

    next revision

---
+++ alembic/versions/78f1fc77d97f_serial_id_column.py
@@ -0,0 +1,24 @@
+"""serial id column
+
+Revision ID: 78f1fc77d97f
+Revises: f2e6d2f2bb3f
+Create Date: 2022-06-30 13:04:45.188741
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '78f1fc77d97f'
+down_revision = 'f2e6d2f2bb3f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
