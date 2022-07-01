1224aa19861a14b99003a08d9e278c71d6acc8a9
eminent alpaca <kristin@northcountrychapel.com>
2022-07-01 14:49:21 -0700

    skip and limit

--- app/routers/networks.py
+++ app/routers/networks.py
@@ -15,11 +15,13 @@ today = datetime.date.today().isoformat()
 
 #get list of programs on one network
 @router.get("/{network}", summary="List programs on one network: CALVARY or ACN")
-def get_network(network, response: Response, db: Session= Depends(get_db), search: Optional[str]=None):
+def get_network(network, response: Response, db: Session= Depends(get_db), search: Optional[str]=None, skip: int = 0, limit: int = 20):
     """
     List programs by network (CALVARY or ACN). Returned in reverse chronological order from today's date.
 
-    Search within network with network?search=
+    Returns 20 programs (one month) by default. Use: ?limit= to get more results. ?skip= to get earlier results.
+
+    Search within network for titles with network?search=
 
     For future programs use "Get programs by network and date" endpoint: /v1/{network}/{start_date}?end_date=
     """
@@ -27,9 +29,9 @@ def get_network(network, response: Response, db: Session= Depends(get_db), searc
 
     #sqlalchemy doesn't like ilike with  None types so don't make it.
     if search is None:
-        programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).all()
+        programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).limit(limit).offset(skip).all()
     else:
-        programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).filter(models.APWI.title.ilike(f'%{search}%')).order_by(desc(models.APWI.airdate)).all() 
+        programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).filter(models.APWI.title.ilike(f'%{search}%')).order_by(desc(models.APWI.airdate)).limit(limit).offset(skip).all()
 
     if not programs:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
@@ -39,7 +41,7 @@ def get_network(network, response: Response, db: Session= Depends(get_db), searc
 @router.get("/{network}/{start_date}", summary="Get programs by network and date")
 def get_network_dates (network: str,  response: Response, db: Session= Depends(get_db), start_date: str = Query(regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"), end_date: Optional[str] = Query(default=None, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")):
     """
-    Get programs on a specific network (CALVARY or ACN) by date (YYYY-MM-DD).  
+    Get programs on a specific network (CALVARY or ACN) by date (YYYY-MM-DD).
     
     To return a range add ?end_date="YYYY-MM-DD" to query. Returned in reverse chronological order.
     """
