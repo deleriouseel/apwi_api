ecda32da96f816437fefef258e94df0958dce588
eminent alpaca <kristin@northcountrychapel.com>
2022-07-01 13:47:11 -0700

    working with search

--- app/routers/networks.py
+++ app/routers/networks.py
@@ -16,17 +16,19 @@ today = datetime.date.today().isoformat()
 
 #get list of programs on one network
 @router.get("/{network}", summary="List programs on one network: CALVARY or ACN")
-def get_network(network, response: Response, db: Session= Depends(get_db), search: Union[str, None] = Query(default=None)):
+def get_network(network, response: Response, db: Session= Depends(get_db), search: Optional[str]=None):
     """
     List programs by network (CALVARY or ACN). Returned in reverse chronological order from today's date.
+    
     Search within network with network?search=
 
-    For future programs use /v1/{network}/{start_date}?end_date=
+    For future programs use "Get programs by network and date" endpoint: /v1/{network}/{start_date}?end_date=
     """
-
     network = network.upper()
-
-    programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).all()
+    if search is None:
+        programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).all()
+    else:
+        programs = db.query(models.APWI).filter(models.APWI.network == network).filter(models.APWI.airdate <= today).filter(models.APWI.title.ilike(f'%{search}%')).order_by(desc(models.APWI.airdate)).all() 
 
     if not programs:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
@@ -34,7 +36,7 @@ def get_network(network, response: Response, db: Session= Depends(get_db), searc
 
 #get list of programs on a network by date
 @router.get("/{network}/{start_date}", summary="Get programs by network and date")
-def get_network_dates (network: str,  response: Response, db: Session= Depends(get_db), start_date: str = Query(default=Required, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"), end_date: Union[str, None] = Query(default=None, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")):
+def get_network_dates (network: str,  response: Response, db: Session= Depends(get_db), start_date: str = Query(regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"), end_date: Optional[str] = Query(default=None, regex="^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")):
     """
     Get programs on a specific network (CALVARY or ACN) by date (YYYY-MM-DD).  
     
--- app/routers/programs.py
+++ app/routers/programs.py
@@ -16,31 +16,21 @@ today = datetime.date.today().isoformat()
 
 #get list of programs ordered by descending date
 @router.get("/", status_code=status.HTTP_200_OK, summary="Get list of all programs")
-def get_programs(response: Response, db: Session= Depends(get_db), search: Union[str, None] = Query(default=None)):
+def get_programs(response: Response, db: Session= Depends(get_db), search: Optional[str] = Query(default=None)):
     """
     List all Apply Within programs. Returned in reverse chronological order from today's date.
+
+    Search all programs by titles using: /?search=
     """
     
     if search is None:
         programs = db.query(models.APWI).filter(models.APWI.airdate <= today).order_by(desc(models.APWI.airdate)).all()
     else:
-        programs = db.query(models.APWI).filter(models.APWI.airdate <= today).filter(models.APWI.title.ilike({search})).all()
+        programs = db.query(models.APWI).filter(models.APWI.airdate <= today).filter(models.APWI.title.ilike(f'%{search}%')).all()
 
     if not programs:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
     return programs
 
 
-#get list of programs by title
-# @router.get("/{search}", summary="Get programs by book title")
-# def get_titles(response: Response, db: Session= Depends(get_db)):
-#     """
-#     List all Apply Within programs with search term in title.
-#     """
-
-#     programs = db.query(models.APWI).f.order_by(desc(models.APWI.airdate)).all()
-#     print(programs)
-#     if not programs:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Database did not return any programs")
-#     return programs  
 
