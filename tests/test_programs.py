ce1a12109235637da93ccd12e4f028048da7115d
eminent alpaca <kristin@northcountrychapel.com>
2022-07-01 16:54:47 -0700

    test root, network, program endpoints

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -1,3 +1,4 @@
+import datetime
 from fastapi.testclient import TestClient
 from app.main import app
 
@@ -8,3 +9,19 @@ def test_root():
     res = client.get("/")
     assert res.json().get('Hello World') == "Jesus Loves You"
     assert res.status_code == 200
+
+def test_network():
+
+    res = client.get("/v1/acn")
+    assert res.status_code == 200
+
+
+def test_program():
+
+    res = client.get("/v1/programs")
+    airdate = res.json()[0] 
+    today = datetime.date.today().isoformat()
+    
+    assert airdate.get('airdate') == today
+    assert res.status_code == 200
+
