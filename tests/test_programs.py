f7d70ffc1e3da2d32b6a20a5ba0c303bd88274c8
eminent alpaca <kristin@northcountrychapel.com>
2022-07-01 17:53:23 -0700

    moar tests

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -10,9 +10,35 @@ def test_root():
     assert res.json().get('Hello World') == "Jesus Loves You"
     assert res.status_code == 200
 
-def test_network():
+def test_network_acn():
 
     res = client.get("/v1/acn")
+    network = res.json()[0]
+
+    assert network.get('network')  == 'ACN'
+    assert res.status_code == 200
+
+def test_network_calvary():
+
+    res = client.get("/v1/calvary")
+    network = res.json()[0]
+
+    assert network.get('network')  == 'CALVARY'
+    assert res.status_code == 200 
+
+def test_network_search():
+
+    res = client.get("/v1/acn?search=peter")
+    search = res.json()[0].get('title')
+    
+    assert "Peter" in search
+    assert res.status_code == 200
+
+def test_network_date():
+    res = client.get("/v1/acn/2022-03-03")
+    date = res.json()[0].get('airdate')
+   
+    assert "2022-03-03" in date
     assert res.status_code == 200
 
 
@@ -24,4 +50,12 @@ def test_program():
     
     assert airdate.get('airdate') == today
     assert res.status_code == 200
+
+def test_program_search():
+
+    res = client.get("/v1/programs?search=colossians")
+    search = res.json()[0].get('title')
+    
+    assert "Colossians" in search
+    assert res.status_code == 200       
 
