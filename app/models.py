9724582649b14df254d035dcf9b7a902ef048976
deleriouseel <kristin@northcountrychapel.com>
2023-12-14 16:12:57 -0800

    station, airtime, location updates

--- app/models.py
+++ app/models.py
@@ -29,10 +29,19 @@ class STATION(Base):
     __tablename__ = "station"
 
     idStation = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    frequency = Column(String, nullable=False)
+    location = Column(
+        Integer, ForeignKey("location.idLocation", ondelete="CASCADE"), nullable=False
+    )
+    airtime = Column(
+        Integer, ForeignKey("airtime.idAirtime", ondelete="CASCADE"), nullable=False
+    )
+    live = Column(Boolean, nullable=False, server_default="0")
     network = Column(String, nullable=True)
     url = Column(String, nullable=True)
     image = Column(String, nullable=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=True)
+    callLetters = Column(String, nullable=True)
 
 
 class AIRTIME(Base):
@@ -40,26 +49,18 @@ class AIRTIME(Base):
 
     idAirtime = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     time = Column(Time, nullable=False)
-    airdays = Column(String, nullable=False)
-    live = Column(Boolean, nullable=False)
+    airdays = Column(String, nullable=False)  # weekdays, saturday, sunday
 
 
 class LOCATION(Base):
     __tablename__ = "location"
 
-    idLocation = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    idLocation: Optional[int] = Column(
+        Integer, primary_key=True, nullable=False, autoincrement=True
+    )
     city = Column(String, nullable=False)
     state = Column(String, nullable=False)
     country = Column(String, nullable=False)
-
-
-class CALLLETTERS(Base):
-    __tablename__ = "callLetters"
-
-    idCallLetters = Column(
-        Integer, primary_key=True, nullable=False, autoincrement=True
-    )
-    callLetters = Column(String, nullable=False)
 
 
 class User(Base):
